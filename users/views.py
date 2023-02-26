e4341a03fe757c9f11ca9135359c3cb2621a61da
yuhuikim <yuheecong@naver.com>
2023-02-26 13:02:31 +0900

    12.3 Log In and Log Out

--- users/views.py
+++ users/views.py
@@ -8,6 +8,7 @@ from rest_framework.permissions import IsAuthenticated
 from users.models import User
 from . import serializers
 
+
 class Me(APIView):
     permission_classes = [IsAuthenticated]
 
@@ -18,7 +19,7 @@ class Me(APIView):
 
     def put(self, request):
         user = request.user
-        # db에 있는 instance로 serializer를 만들고, 
+        # db에 있는 instance로 serializer를 만들고,
         # user에게서 온 data고
         # 이것들을 serializer에게 주면 알아서 업데이트 해준다.
         serializer = serializers.PrivateUserSerializer(
@@ -34,6 +35,7 @@ class Me(APIView):
         else:
             return Response(serializer.errors)
 
+
 class Users(APIView):
     def post(self, request):
         # password가 포함된 data를 받자. --> user가 보낸 password 를 가지고 오는 부분! -- step 1
@@ -41,22 +43,25 @@ class Users(APIView):
         if not password:
             raise ParseError
         # serializer은 PrivateUserSerializer를 가져올거고, user가 보낸 data 도 가져온다.
-        serializer = serializers.PrivateUserSerializer(data=request.data, )
+        serializer = serializers.PrivateUserSerializer(
+            data=request.data,
+        )
         if serializer.is_valid():
-            user = serializer.save() # db에 저장하는 부분
+            user = serializer.save()  # db에 저장하는 부분
             # user.password = password # raw password를 그대로 저장하는 방법
-            user.set_password(password) # hash화 된 password를 저장하기 위하여
+            user.set_password(password)  # hash화 된 password를 저장하기 위하여
             user.save()
             # 새로운 user를 받는다..!
             serializer = serializers.PrivateUserSerializer(user)
             return Response(serializer.data)
-        else: 
+        else:
             return Response(serializer.errors)
+
 
 # http://127.0.0.1:8000/api/v1/users/@admin1
 class PublicUser(APIView):
     def get(self, request, username):
-        try: 
+        try:
             user = User.objects.get(username=username)
         except User.DoesNotExist:
             raise NotFound
@@ -81,6 +86,37 @@ class ChangePassword(APIView):
             # 그러니까 아래의 작업이 꼭 필요하다.. --> 왜?
             user.save()
             return Response(status=status.HTTP_200_OK)
-        else: 
+        else:
             raise ParseError
 
+
+class LogIn(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        if not username or not password:
+            raise ParseError
+        # username과 password이 같으면 user가 나옴!
+        user = authenticate(
+            request,
+            username=username,
+            password=password,
+        )
+        # user를 찾으면 request 객체와 login 함수를 호출하면 된다.
+        if user:
+            # login 함수를 호출하면 장고에서는 user를 로그인 시키고, 백엔드에서는 user가 담긴 세션을 생성하고, 사용자에게 쿠키를 보내준다.
+            login(
+                request,
+                user,
+            )
+            return Response({"ok":"Welcome!"})
+        else:
+            return Response({"error": "wrong password"})
+
+class LogOut(APIView):
+    
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        logout(request)
+        return Response(({"ok":"GoodBye"}))
